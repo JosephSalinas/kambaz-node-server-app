6fcb9d66a156fcefd3df551aab20a63bb6fa7084
JosephSalinas <75646269+JosephSalinas@users.noreply.github.com>
2025-04-03 07:54:51 -0400

    done2

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -15,6 +15,12 @@ export default function WorkingWithArrays(app) {
         res.json(todos);
     });
 
+    app.get("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        res.json(todo);
+    });
+
     app.get("/lab5/todos", (req, res) => {
         const { completed } = req.query;
         if (completed !== undefined) {
@@ -66,6 +72,16 @@ export default function WorkingWithArrays(app) {
         todos.push(newTodo);
         res.json(newTodo);
     });
+    app.get("/lab5/todos/create", (req, res) => {
+        const newTodo = {
+            id: new Date().getTime(),
+            title: "New Task",
+            completed: false,
+        };
+        todos.push(newTodo);
+        res.json(todos);
+    });
+
     app.delete("/lab5/todos/:id", (req, res) => {
         const { id } = req.params;
         const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
